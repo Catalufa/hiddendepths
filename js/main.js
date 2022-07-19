1a0b1f127ae4c25b77b447bc923c639692d2da8b
Catalufa <79006629+Catalufa@users.noreply.github.com>
2022-07-19 12:04:17 +0100

    Upload new landing

---
+++ js/main.js
@@ -0,0 +1,5 @@
+particlesJS.load('particles-js', 'assets/particles.json');
+
+document.getElementById("contact").addEventListener("click", function() {
+  window.open("mailto:contact@hiddendepths.uk")
+});
