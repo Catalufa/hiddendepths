20f51c556a5a77b529962c27b6b8ad9e0afa882f
Catalufa <79006629+Catalufa@users.noreply.github.com>
2024-03-10 22:29:19 +0000

    Delete js directory

--- js/main.js
+++
@@ -1,5 +0,0 @@
-particlesJS.load('particles-js', 'assets/particles.json');
-
-document.getElementById("contact").addEventListener("click", function() {
-  window.open("mailto:contact@hiddendepths.uk")
-});
